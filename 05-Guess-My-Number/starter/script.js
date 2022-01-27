95688219ba81f3a7ff85b95c1c8bf1d9395a3d72
Divmrley <nexted.marley@gmail.com>
2022-01-27 15:01:18 +0000

    guess game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,6 +2,34 @@
 // all variables here
 
 const myTitle = document.querySelector('#title');
-
-myTitle.innerHTML ="uirhfsughugdgud;udg"
+const checkBtn = document.querySelector('.check');
+const innputField = document.querySelector('.guess');
+const snumber = document.querySelector('.number');
+const mymessage = document.querySelector('.message');
+let randomnumber =Math.trunc(Math.random()*20)+1;
 // end variables
+snumber.innerHTML = randomnumber
+ 
+
+
+
+checkBtn.addEventListener('click',function(){ 
+    if (!innputField.value) {
+        mymessage.innerHTML ='empty';
+        document.querySelector('body').style.backgroundColor  ="red";
+
+    } else if (randomnumber==innputField.value) {
+        mymessage.innerHTML ='correct';
+        document.querySelector('body').style.backgroundColor  ="green";
+    }else{ 
+        mymessage.innerHTML ='not correct';
+        document.querySelector('body').style.backgroundColor  ="red";
+
+    }
+        
+  
+
+    
+
+
+});
